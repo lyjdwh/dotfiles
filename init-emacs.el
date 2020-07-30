441c1a8898da932e3a5a6ab0f69dd69bbaf264b5
lyjdwh <1412511544@qq.com>
2020-07-30 20:03:01 +0800

    add the minimum emacs configuration

---
+++ init-emacs.el
@@ -0,0 +1,16 @@
+;; init-emacs.el
+;; emacs -Q -l init-emacs.el
+
+(setq package-enable-at-startup nil)
+(setq package-archives
+      '(("melpa-cn" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+        ("org-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
+        ("gnu-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
+
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
