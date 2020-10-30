ccb836ca6cdc14393258da30ad10278b92f9ac98
lyjdwh <1412511544@qq.com>
2020-10-30 15:06:02 +0800

    add error handling for watching repo

--- scripts/update_watched_repo.py
+++ scripts/update_watched_repo.py
@@ -5,7 +5,6 @@ import subprocess
 
 basedir = "~/bin"
 watched_repos = ["emacs-application-framework",
-                 "gk-habit",
                  "inherit-org",
                  "notdeft",
                  "org-clock-watch",
@@ -14,5 +13,9 @@ watched_repos = ["emacs-application-framework",
 for repo in watched_repos:
     path = os.path.join(basedir, repo)
     cmd = "cd " + path + "&& git pull origin master"
-    output = subprocess.check_output([cmd],  shell=True)
-    print(output)
+    try:
+        output = subprocess.check_output([cmd],  shell=True)
+    except:
+        pass
+    finally:
+        print(output)
