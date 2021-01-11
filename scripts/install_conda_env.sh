86b99bc8a87d3c5e1884a713a40cc57c08b897c9
lyjdwh <1412511544@qq.com>
2021-01-11 20:11:26 +0800

    add conda env script

---
+++ scripts/install_conda_env.sh
@@ -0,0 +1,18 @@
+conda create -y -n tftorch python=3.8
+conda activate tftorch
+# tensorflow
+conda install -y tensorflow-gpu
+# pytorch
+conda install -y pytorch torchvision torchaudio cudatoolkit=10.1 -c pytorch
+# pytorch lightning
+conda install -y pytorch-lightning -c conda-forge
+# pyg
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.7.0+cu101.html
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.7.0+cu101.html
+pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.7.0+cu101.html
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.7.0+cu101.html
+pip install torch-geometric
+# others
+conda install -y jupyter biopython seaborn py3Dmol
+pip install ptipython black isort
+pip install biopandas torchsnooper loguru optuna keras-tuner pretty_errors omegaconf
