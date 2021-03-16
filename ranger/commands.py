3856851c7d0c61d72c9460f71f6ec4c71e1e33f5
lyjdwh <1412511544@qq.com>
2021-03-16 21:59:48 +0800

    and upload and yank file for ranger

--- ranger/commands.py
+++ ranger/commands.py
@@ -1,9 +1,12 @@
 import os
 import re
 import signal
+import subprocess
 
 from ranger.api.commands import Command
 from ranger.core.loader import CommandLoader
+from ranger.container.file import File
+from ranger.ext.get_executables import get_executables
 
 
 class paste_as_root(Command):
@@ -358,3 +361,49 @@ class tmux_detach(Command):
         if not os.environ.get('TMUX'):
             return
         os.system("tmux detach")
+
+class up(Command):
+    def execute(self):
+        if self.arg(1):
+            scpcmd = ["scp"]
+            scpcmd.extend([f.realpath for f in self.fm.thistab.get_selection()])
+            scpcmd.append(self.arg(1))
+            self.fm.execute_command(scpcmd)
+            self.fm.notify("Uploaded!")
+
+class YankContent(Command):
+    """
+    Copy the content of image file and text file with xclip
+    """
+
+    def execute(self):
+        if 'xclip' not in get_executables():
+            self.fm.notify('xclip is not found.', bad=True)
+            return
+
+        arg = self.rest(1)
+        if arg:
+            if not os.path.isfile(arg):
+                self.fm.notify('{} is not a file.'.format(arg))
+                return
+            file = File(arg)
+        else:
+            file = self.fm.thisfile
+            if not file.is_file:
+                self.fm.notify('{} is not a file.'.format(file.relative_path))
+                return
+
+        relative_path = file.relative_path
+        cmd = ['xclip', '-selection', 'clipboard']
+        if not file.is_binary():
+            with open(file.path, 'rb') as fd:
+                subprocess.check_call(cmd, stdin=fd)
+        elif file.image:
+            cmd += ['-t', file.mimetype, file.path]
+            subprocess.check_call(cmd)
+            self.fm.notify('Content of {} is copied to x clipboard'.format(relative_path))
+        else:
+            self.fm.notify('{} is not an image file or a text file.'.format(relative_path))
+
+    def tab(self, tabnum):
+        return self._tab_directory_content()
